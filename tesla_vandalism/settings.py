a544cfe3be3ededb2767fa2d8a610af2ff2bfc5d
jonahliu <jia7833@hotmail.com>
2025-03-27 11:36:34 -0400

    add .heroku.com to allowed_hosts for heroku deployment

--- tesla_vandalism/settings.py
+++ tesla_vandalism/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3p1h#-eant(dq6(&30*+ffk(ezpc+t6$_-q896h4%%s2ugg!30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
